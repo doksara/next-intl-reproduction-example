8f3ec6b287771a8c182656e2d0fba9f3e89413b8
Darjan <darjan.baricevic@gmail.com>
2025-03-03 14:00:54 +0100

    gitignore

--- app/(routes)/[locale]/layout.tsx
+++ app/(routes)/[locale]/layout.tsx
@@ -1,17 +1,17 @@
-import { routing } from '@/i18n/routing';
-import { PropsWithChildren } from 'react';
-import { setRequestLocale, getMessages } from 'next-intl/server';
+import { routing } from "@/i18n/routing"
+import { PropsWithChildren } from "react"
+import { setRequestLocale, getMessages } from "next-intl/server"
 
-import { NextIntlClientProvider } from 'next-intl';
+import { NextIntlClientProvider } from "next-intl"
 
-import { ScrollDirectionProvider } from '@/providers/ScrollProvider';
-import { Header } from '@/layout/Header';
+import { ScrollDirectionProvider } from "@/providers/ScrollProvider"
+import { Header } from "@/layout/Header"
 
-import '@/styles/index.css';
-import { notFound } from 'next/navigation';
+import "@/styles/index.css"
+import { notFound } from "next/navigation"
 
 export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
+  return routing.locales.map((locale) => ({ locale }))
 }
 
 export default async function LocaleLayout({
@@ -19,16 +19,16 @@ export default async function LocaleLayout({
   params,
 }: PropsWithChildren & { params: Promise<{ locale: string }> }) {
   // Ensure that the incoming `locale` is valid
-  const { locale } = await params;
+  const { locale } = await params
 
   if (!routing.locales.includes(locale as any)) {
-    notFound();
+    notFound()
   }
 
-  setRequestLocale(locale);
+  setRequestLocale(locale)
   // Providing all messages to the client
   // side is the easiest way to get started
-  const messages = await getMessages();
+  const messages = await getMessages()
 
   return (
     <html lang={locale}>
@@ -42,5 +42,5 @@ export default async function LocaleLayout({
         </NextIntlClientProvider>
       </body>
     </html>
-  );
+  )
 }
